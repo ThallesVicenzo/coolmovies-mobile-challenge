8609477dd76d94e608c9f8a3a2b42bd645d37557
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-03-27 16:00:39 -0300

    feat: adds LoginModel

---
+++ lib/features/login/infra/models/login_model.dart
@@ -0,0 +1,11 @@
+import 'package:coolmovies/features/login/domain/entities/login_entity.dart';
+
+class LoginModel extends LoginEntity {
+  LoginModel({required super.name});
+
+  factory LoginModel.fromJson(Map<String, dynamic>? json) {
+    return LoginModel(
+      name: json?['name'],
+    );
+  }
+}
