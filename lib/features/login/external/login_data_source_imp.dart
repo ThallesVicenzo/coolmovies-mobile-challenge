31b75c691b6cdf88f2db2115ee6d0d0f378b1083
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-03-28 16:32:42 -0300

    feat: adds LoginDataSourceImpl

---
+++ lib/features/login/external/login_data_source_imp.dart
@@ -0,0 +1,34 @@
+import 'package:coolmovies/features/login/domain/entities/login_entity.dart';
+import 'package:coolmovies/features/login/infra/models/login_model.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import '../infra/data_source/pokedata_data_source.dart';
+
+class LoginDataSourceImpl implements LoginDataSource {
+  final GraphQLClient client;
+
+  LoginDataSourceImpl({
+    required this.client,
+  });
+
+  @override
+  Future<LoginEntity> call(String name) async {
+    final QueryResult result = await client.mutate(
+      MutationOptions(
+        variables: {'name': name},
+        document: gql('''
+           mutation createUser(\$name: String!) {
+              createUser(input: {user: {name: \$name}}) {
+               user {
+                  id
+                  name
+                }
+              }
+             }
+        '''),
+      ),
+    );
+
+    return LoginModel.fromJson(result.data);
+  }
+}
