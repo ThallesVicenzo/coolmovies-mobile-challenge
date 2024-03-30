8f6cab87aa5bf7791d2037a20905e3581ecbc4df
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-03-30 12:26:24 -0300

    feat: adds CoolMoviesText

---
+++ app_design/lib/widgets/cool_movies_text.dart
@@ -0,0 +1,27 @@
+import 'package:app_design/widgets/colors_palette.dart';
+import 'package:flutter/material.dart';
+
+class CoolMoviesText extends StatelessWidget {
+  const CoolMoviesText({
+    super.key,
+    required this.text,
+    required this.size,
+    this.fontWeight = FontWeight.normal,
+  });
+
+  final String text;
+  final double size;
+  final FontWeight fontWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: ColorsPalette.marfim,
+        fontWeight: fontWeight,
+        fontSize: size,
+      ),
+    );
+  }
+}
