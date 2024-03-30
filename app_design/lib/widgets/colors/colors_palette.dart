24ac8662b3c0bc8e38feb79fd2e40aee920b8f81
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-03-30 16:24:28 -0300

    feat: adds new color

--- app_design/lib/widgets/colors/colors_palette.dart
+++ app_design/lib/widgets/colors/colors_palette.dart
@@ -5,6 +5,7 @@ class ColorsPalette extends Color {
   static const Color saffron = Color(0xffd6a35c);
   static const Color darkWhite = Color(0xffebd1ad);
   static const Color marfim = Color(0xfffefcff);
+  static const Color normalGray = Color(0xFF787879);
 
   ColorsPalette(super.value);
 }
