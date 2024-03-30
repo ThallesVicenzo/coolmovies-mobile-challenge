3589b7323497733eb2299825995a8478ddc0ba72
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-03-30 12:26:06 -0300

    feat: adds final values

---
+++ app_design/lib/utils/values/finals.dart
@@ -0,0 +1,7 @@
+import 'package:app_design/widgets/colors_palette.dart';
+import 'package:flutter/material.dart';
+
+final outlineBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(10),
+  borderSide: const BorderSide(color: ColorsPalette.saffron),
+);
