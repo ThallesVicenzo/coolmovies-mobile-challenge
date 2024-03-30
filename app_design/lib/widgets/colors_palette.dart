965eadc9b9d4a6e6ee003ce4b2d0c1928e24dd3c
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-03-30 12:25:50 -0300

    feat: adds Colors methods

--- app_design/lib/widgets/colors_palette.dart
+++ app_design/lib/widgets/colors_palette.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-class ColorsPalette {
+class ColorsPalette extends Color {
   static const Color backgroundColor = Color(0xff161616);
   static const Color saffron = Color(0xffd6a35c);
   static const Color darkWhite = Color(0xffebd1ad);
   static const Color marfim = Color(0xfffefcff);
+
+  ColorsPalette(super.value);
 }
