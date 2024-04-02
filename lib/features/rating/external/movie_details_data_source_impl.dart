d357be6b5b22ac0c5cb0aa87b8ebf6ac1891304b
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-02 11:24:14 -0300

    feat: adds movie details external

---
+++ lib/features/rating/external/movie_details_data_source_impl.dart
@@ -0,0 +1,46 @@
+import 'package:coolmovies/features/rating/domain/entity/movie_details.dart';
+import 'package:coolmovies/features/rating/infra/data_source/movie_details_data_source.dart';
+import 'package:coolmovies/features/rating/infra/models/movie_details_model.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class MovieDetailsDataSourceImpl implements MovieDetailsDataSource {
+  final GraphQLClient client;
+
+  MovieDetailsDataSourceImpl({required this.client});
+
+  @override
+  Future<MovieDetailsEntity> call(String id) async {
+    final QueryResult result = await client.query(
+      QueryOptions(
+        variables: {
+          'id': id,
+        },
+        document: gql('''
+          query GetMovieById('\$id: String!') {
+            movieById(id: \$id) {
+              imgUrl
+              title
+              releaseDate
+              movieDirectorByMovieDirectorId {
+                name
+              }
+              movieReviewsByMovieId {
+                nodes {
+                  body
+                  rating
+                  title
+                  userByUserReviewerId {
+                    name
+                  }
+                }
+              }
+            }
+          } 
+        '''),
+      ),
+    );
+    final data = result.data?['movieById'];
+
+    return MovieDetailsModel.fromJson(data);
+  }
+}
