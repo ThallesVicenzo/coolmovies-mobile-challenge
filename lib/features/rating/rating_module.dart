29b4c8941a8dbda4bd8fc0c2c71f14c7588d20e1
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-03 12:36:15 -0300

    feat: injects create review dependencies

--- lib/features/rating/rating_module.dart
+++ lib/features/rating/rating_module.dart
@@ -1,14 +1,19 @@
 import 'package:coolmovies/features/rating/domain/entity/movie_reviews.dart';
+import 'package:coolmovies/features/rating/domain/repository/create_review_repository.dart';
 import 'package:coolmovies/features/rating/domain/repository/movie_details_repository.dart';
+import 'package:coolmovies/features/rating/domain/usecases/create_review_usecase.dart';
 import 'package:coolmovies/features/rating/domain/usecases/movie_details_usecase.dart';
 import 'package:coolmovies/features/rating/external/movie_details_data_source_impl.dart';
+import 'package:coolmovies/features/rating/infra/data_source/create_review_data_source.dart';
 import 'package:coolmovies/features/rating/infra/data_source/movie_details_data_source.dart';
+import 'package:coolmovies/features/rating/infra/repository/create_review_repository_impl.dart';
 import 'package:coolmovies/features/rating/infra/repository/movie_details_repository_impl.dart';
 import 'package:coolmovies/features/rating/presenter/pages/all_reviews_page.dart';
 import 'package:coolmovies/features/rating/presenter/pages/movie_details_page.dart';
 import 'package:coolmovies/features/rating/rating_routes.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'external/create_review_data_source_impl.dart';
 import 'presenter/controllers/movie_details_controller.dart';
 
 class RatingModule extends Module {
@@ -29,9 +34,27 @@ class RatingModule extends Module {
             repository: i<MovieDetailsRepository>(),
           ),
         ),
+        Bind.factory<CreateReviewDataSource>(
+          (i) => CreateReviewDataSourceImpl(
+            client: i(),
+            secureStorage: i(),
+          ),
+        ),
+        Bind.factory<CreateReviewRepository>(
+          (i) => CreateReviewRepositoryImpl(
+            dataSource: i<CreateReviewDataSource>(),
+          ),
+        ),
+        Bind.factory<CreateReviewUseCase>(
+          (i) => CreateReviewUseCaseImpl(
+            repository: i<CreateReviewRepository>(),
+          ),
+        ),
         Bind.factory<MovieDetailsController>(
           (i) => MovieDetailsController(
-            useCase: i<MovieDetailsUseCase>(),
+            detailsUsecase: i<MovieDetailsUseCase>(),
+            commentUseCase: i<CreateReviewUseCase>(),
+            secureStorage: i(),
           ),
         ),
       ];
