70f4dc20291a1a3603fb237bfc26ddae8c2f60ce
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-03 12:35:20 -0300

    fix: fixes small bug

--- lib/features/rating/presenter/pages/all_reviews_page.dart
+++ lib/features/rating/presenter/pages/all_reviews_page.dart
@@ -18,58 +18,61 @@ class _AllReviewsPageState extends State<AllReviewsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CoolMoviesAppBar(),
-      body: Column(
-        children: [
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: widget.entity.length,
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: ColorsPalette.marfim),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CoolMoviesText(
-                            text: widget.entity[index].title,
-                            size: 16,
-                            fontWeight: FontWeight.w700,
-                          ),
-                          CoolMoviesText(
-                            text: '${widget.entity[index].rating}/5',
-                            size: 16,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: CoolMoviesText(
-                          text: widget.entity[index].user.name,
-                          size: 16,
-                          fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: widget.entity.length,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: ColorsPalette.marfim),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CoolMoviesText(
+                              text: widget.entity[index].title,
+                              size: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            CoolMoviesText(
+                              text: '${widget.entity[index].rating}/5',
+                              size: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ],
                         ),
-                      ),
-                      CoolMoviesText(
-                        text: widget.entity[index].body,
-                        size: 14,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: CoolMoviesText(
+                            text: widget.entity[index].user.name,
+                            size: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        CoolMoviesText(
+                          text: widget.entity[index].body,
+                          size: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
