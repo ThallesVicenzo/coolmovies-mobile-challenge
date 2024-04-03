6f7aebfd7b715bf569e96dbb83be3788f5ce63b2
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-03 14:50:55 -0300

    feat: adds small logic to list view

--- lib/features/rating/presenter/widgets/other_reviews_preview.dart
+++ lib/features/rating/presenter/widgets/other_reviews_preview.dart
@@ -13,39 +13,37 @@ class OtherReviewsPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(maxHeight: 200),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemCount: 2,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.only(bottom: 16, left: 8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CoolMoviesText(
-                    text: entity.movieReviewsByMovieId[index].title,
-                    size: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  CoolMoviesText(
-                    text:
-                        '${entity.movieReviewsByMovieId[index].rating.toString()}/5',
-                    size: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ],
-              ),
-              CoolMoviesText(
-                text: entity.movieReviewsByMovieId[index].user.name,
-                size: 20,
-                fontWeight: FontWeight.w500,
-              ),
-            ],
-          ),
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      itemCount: entity.movieReviewsByMovieId.length < 3 ? 2 : 3,
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.only(bottom: 16, left: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CoolMoviesText(
+                  text: entity.movieReviewsByMovieId[index].title,
+                  size: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+                CoolMoviesText(
+                  text:
+                      '${entity.movieReviewsByMovieId[index].rating.toString()}/5',
+                  size: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ],
+            ),
+            CoolMoviesText(
+              text: entity.movieReviewsByMovieId[index].user.name,
+              size: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ],
         ),
       ),
     );
