c991956def5a3cde9120d703d958fb414e364340
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-04-03 15:53:26 -0300

    feat: adds delete reviews

--- lib/features/rating/presenter/widgets/other_reviews_preview.dart
+++ lib/features/rating/presenter/widgets/other_reviews_preview.dart
@@ -1,3 +1,4 @@
+import 'package:app_design/widgets/colors/colors_palette.dart';
 import 'package:app_design/widgets/text/cool_movies_text.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +8,13 @@ class OtherReviewsPreview extends StatelessWidget {
   const OtherReviewsPreview({
     super.key,
     required this.entity,
+    this.confirmDismiss,
+    required this.id,
   });
 
   final MovieDetailsEntity entity;
+  final Future<bool?> Function(DismissDirection)? confirmDismiss;
+  final String id;
 
   @override
   Widget build(BuildContext context) {
@@ -19,31 +24,54 @@ class OtherReviewsPreview extends StatelessWidget {
       itemCount: entity.movieReviewsByMovieId.length < 3 ? 2 : 3,
       itemBuilder: (context, index) => Padding(
         padding: const EdgeInsets.only(bottom: 16, left: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Dismissible(
+          key: UniqueKey(),
+          direction: DismissDirection.endToStart,
+          confirmDismiss: confirmDismiss,
+          background: Container(
+            decoration: const BoxDecoration(
+              color: ColorsPalette.saffron,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(4),
+              ),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                CoolMoviesText(
-                  text: entity.movieReviewsByMovieId[index].title,
-                  size: 20,
-                  fontWeight: FontWeight.w500,
+                Icon(
+                  Icons.delete_outline,
+                  color: ColorsPalette.marfim,
                 ),
-                CoolMoviesText(
-                  text:
-                      '${entity.movieReviewsByMovieId[index].rating.toString()}/5',
-                  size: 20,
-                  fontWeight: FontWeight.w500,
-                ),
+                SizedBox(width: 16),
               ],
             ),
-            CoolMoviesText(
-              text: entity.movieReviewsByMovieId[index].user.name,
-              size: 20,
-              fontWeight: FontWeight.w500,
-            ),
-          ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CoolMoviesText(
+                    text: entity.movieReviewsByMovieId[index].title,
+                    size: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  CoolMoviesText(
+                    text:
+                        '${entity.movieReviewsByMovieId[index].rating.toString()}/5',
+                    size: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ],
+              ),
+              CoolMoviesText(
+                text: entity.movieReviewsByMovieId[index].user.name,
+                size: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ],
+          ),
         ),
       ),
     );
